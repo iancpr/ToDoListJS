bde1dccf601e980967f245b78d69a9c36272e2ee
Ian Premadasa <ian@firstpointsolutions.com>
2019-04-13 17:01:01 +0100

    added to do colours

--- app.js
+++ app.js
@@ -41,8 +41,10 @@ function addNewTask(){
 
 function formatTodo(item){
 	var listItem = document.createElement('li');
+	
 	var itemOne = document.createElement('span');
 	var itemTwo = document.createElement('span');
+	
 	
 	var taskContent = document.createTextNode(item.task);
 	var dateContent = document.createTextNode(item.date);
@@ -53,14 +55,38 @@ function formatTodo(item){
 	listItem.appendChild(itemOne);
 	listItem.appendChild(itemTwo);
 	listItem.setAttribute('id', item.id);
+	
+	var markButton = document.createElement('button');
+	var buttonText = document.createTextNode('Mark Status');
+	
+	markButton.appendChild(buttonText);
+	markButton.addEventListener('click', function(event){
+		markAsDone(event.target.parentElement.id)
+	})
+	
+	listItem.appendChild(markButton);
+	
 	return listItem;
+}
+
+function markAsDone(itemId){
+	var toMark = todoArray[itemId - 1];
+	toMark.done = !toMark.done;
+	updateTodosView();
 }
 
 
 function updateTodosView (){
 	var list = document.createElement('ul');
 	todoArray.forEach(function(item){
+	if (item.done) {
+		item.setAttribute('class', 'item-done');
+	}	else {
+		item.setAttribute('class', 'item-todo');
+	}
+		
 		list.appendChild(item);
 	});
+	
 	listArea.replaceChild(list, listArea.firstChild);
 }
