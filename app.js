5f5e830f6aef34054c69b04adef2bb220b833b75
Ian Premadasa <ian@firstpointsolutions.com>
2019-04-13 12:31:07 +0100

    updated JS file
    array reference added

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 var taskInput = document.GetElementByID("task");
 var submitButton = document.getElementByID("submitButton");
 var divList = document.getElementByID("myToDoList");
+
+// Creating the event listener. The submitButton is taken from the variable above
+// The add event listener takes 2 arguments, the click and an insulator function which fires another funciton "Add new task"
+submitButton.addEventListener('click' , function(){
+	addNewTask();
+});
+
+var todoArray = [];
