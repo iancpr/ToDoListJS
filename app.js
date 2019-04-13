af96dd8bc2c689e2063213d3f0dc75c403c4738a
Ian Premadasa <ian@firstpointsolutions.com>
2019-04-13 13:13:27 +0100

    update JS file
    console logs added

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var divList = document.getElementByID("myToDoList");
 // Creating the event listener. The submitButton is taken from the variable above
 // The add event listener takes 2 arguments, the click and an insulator function which fires another funciton "addNewTask"
 submitButton.addEventListener('click' , function(){
+	console.log("button clicked");
 	addNewTask();
 });
 
@@ -14,7 +15,7 @@ function addNewTask(){
 //Stores the current date using the inbuilt Javascript funtion - Date	
 	var date = Date();
 	var task = taskInput.value;
-	
+	console.log(task);
 	var todo = {
 		id: todoArray.length + 1,
 		// takes value of variable task and inserts into object
@@ -26,7 +27,7 @@ function addNewTask(){
 	}
 	
 	todoArray.push(todo.task);
-	
+	console.log(todoArray);
 	todoArray.forEach(function(item) {
 		divList.appendChild("<p>" + item + "</p>");
 	});
