6d5afdaee25bd51b9c26a477077ca85a398b2725
Ian Premadasa <ian@firstpointsolutions.com>
2019-04-13 13:09:10 +0100

    updated JS file
    JS file added with object and push

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
-var taskInput = document.GetElementByID("task");
+var taskInput = document.GetElementByID("userTask");
 var submitButton = document.getElementByID("submitButton");
 var divList = document.getElementByID("myToDoList");
 
 // Creating the event listener. The submitButton is taken from the variable above
-// The add event listener takes 2 arguments, the click and an insulator function which fires another funciton "Add new task"
+// The add event listener takes 2 arguments, the click and an insulator function which fires another funciton "addNewTask"
 submitButton.addEventListener('click' , function(){
 	addNewTask();
 });
 
 var todoArray = [];
+
+function addNewTask(){
+//Stores the current date using the inbuilt Javascript funtion - Date	
+	var date = Date();
+	var task = taskInput.value;
+	
+	var todo = {
+		id: todoArray.length + 1,
+		// takes value of variable task and inserts into object
+		task: task,
+		//translates the date into user readable format
+		date: date.toUTCString(),
+		//status of the task the user has created
+		done: false
+	}
+	
+	todoArray.push(todo.task);
+	
+	todoArray.forEach(function(item) {
+		divList.appendChild("<p>" + item + "</p>");
+	});
+	
+	taskInput.value = '';
+}
